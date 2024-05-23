54b54e7873bb35615cbf310c9ff21cf5ff13d3bf
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-05-22 23:23:27 -0300

    chore: add icons

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function page() {
-  return <div>Dashboard Root Page</div>;
+function Page() {
+  return (
+    <div className="bg-red-500 flex-1 h-[calc(100%-80px)] p-6 ">
+      Dashboard Root Page
+    </div>
+  );
 }
 
-export default page;
+export default Page;
