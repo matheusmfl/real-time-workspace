988a4a8270f378fe7660b2fffafc7a7c1456a92c
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-05-24 18:51:12 -0300

    feat: add schemas, and board types, and add mutate on button

--- src/app/(dashboard)/_components/empty-boards.tsx
+++ src/app/(dashboard)/_components/empty-boards.tsx
@@ -1,8 +1,29 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+
+import { api } from "../../../../convex/_generated/api";
+
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutations";
+import { toast } from "sonner";
 
 export function EmptyBoards() {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Quadro criado com sucesso!");
+        // TODO: Redirect to board
+      })
+      .catch(() => toast.error("Putz, algo deu errado!"));
+  };
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src={"/note.svg"} height={110} width={110} alt="empty-favorites" />
@@ -11,7 +32,9 @@ export function EmptyBoards() {
         Comece criando um quadro para sua organização
       </p>
       <div className="mt-6">
-        <Button size={"lg"}>Criar quadro</Button>
+        <Button disabled={pending} onClick={onClick} size={"lg"}>
+          Criar quadro
+        </Button>
       </div>
     </div>
   );
