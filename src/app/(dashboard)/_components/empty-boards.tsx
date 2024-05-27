68083ecfc280cba2c50ecafc8c95fb0922b173c6
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-05-26 23:41:38 -0300

    feat: add search by query string

--- src/app/(dashboard)/_components/empty-boards.tsx
+++ src/app/(dashboard)/_components/empty-boards.tsx
@@ -7,8 +7,10 @@ import { api } from "../../../../convex/_generated/api";
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutations";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export function EmptyBoards() {
+  const router = useRouter();
   const { organization } = useOrganization();
   const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -20,7 +22,7 @@ export function EmptyBoards() {
     })
       .then((id) => {
         toast.success("Quadro criado com sucesso!");
-        // TODO: Redirect to board
+        router.push(`/board/${id}`);
       })
       .catch(() => toast.error("Putz, algo deu errado!"));
   };
